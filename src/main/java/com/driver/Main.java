b3f3c3faa64e62665b100333d983e79f5863c559
Tanmoy Barui <tanmoy.barui.01@gmail.com>
2023-01-13 09:41:14 +0530

    solu 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        A obj= new A();
+        obj.meth();
+        A obj1= new B();
+        obj1.meth();
+
+    }
+}
+class A{
+        void meth(){
+            System.out.println("Invoking method from class A");
+        }
+}
+class B extends A{
+        void meth(){
+            System.out.println("Method is overridden in Extendend class B");
+        }
 }
